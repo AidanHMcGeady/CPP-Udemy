5cd7e76814d1e3b5c026e78f7a9eae76a84c7361
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-14 16:13:44 +0100

    14/09 #3

---
+++ Conditions/4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    int a, b;
+
+    cout << "Enter a: " << endl;
+    cin >> a;
+
+    cout << "Enter b: " << endl;
+    cin >> b;
+
+    if (a > b)
+    {
+        cout << "something" << endl; cout << "test";
+    }
+
+
+}
