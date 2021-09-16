ab622b255f1c24b87d48d7cf8caed0a24d23fea6
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-16 13:37:17 +0100

    16/09 #2
    Ex3.2 Complete

---
+++ Exercises/3-Operators/EXERCISE for Relational Operators/EXERCISE for Relational Operators/Solution/Ex1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Min;
+    int Max;
+    int Mid;
+
+    bool MinMid;
+    bool MaxMid;
+
+    cout << "Test the program! You should input a low number first, a high number second, and a number between them third. But feel free to get this wrong." << endl;
+
+    cout << "Low Number: ";
+    cin >> Min;
+
+    cout << "High Number: ";
+    cin >> Max;
+
+    cout << "Middle Number: ";
+    cin >> Mid;
+
+    if (Mid >= Min)
+    {
+        MinMid = true;
+    }
+
+    if (Mid <= Max)
+    {
+        MaxMid = true;
+    }
+
+    cout << "Is middle number higher than low number? " << MinMid << endl << "Is middle number lower than high number? " << MaxMid << endl;
+}
