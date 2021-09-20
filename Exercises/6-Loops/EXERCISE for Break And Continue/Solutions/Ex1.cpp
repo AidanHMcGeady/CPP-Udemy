0ebd83be864abfc25df7d7035d3aa6ef82ce4dae
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-20 17:03:11 +0100

    20/09 #6

---
+++ Exercises/6-Loops/EXERCISE for Break And Continue/Solutions/Ex1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int userInput = 10;
+int sum;
+
+int main()
+{
+    cout << "Please enter a positive number" << endl;
+
+    cin >> userInput;
+
+    for (int i; i - 1 != userInput; i++)
+    {
+
+
+        if (i % 2 == 0 && i / 2 != 0)
+        {
+            cout << sum << " + " << i << " = ";
+            sum = sum + i;
+            cout << sum << endl;
+        }
+    }
+}
