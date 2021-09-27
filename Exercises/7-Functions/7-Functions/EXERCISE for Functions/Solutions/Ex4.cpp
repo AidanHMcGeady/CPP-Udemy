e88364b635a5e413a9bc4a8818ad54c49ffdce1b
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-27 18:00:16 +0100

    27/09 #4

---
+++ Exercises/7-Functions/7-Functions/EXERCISE for Functions/Solutions/Ex4.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+
+float a, b, c;
+
+int addition()
+{
+    cout << "Input two number that will be added together" << endl;
+
+    cout << "Number 1: " << endl;   cin >> a;
+    cout << "Number 2: " << endl;   cin >> b;
+
+    c = a + b;
+
+    cout << "Added result = " << c << endl;
+}
+
+int subtraction()
+{
+    cout << "Input two number that will be subtracted together" << endl;
+
+    cout << "Number 1: " << endl;   cin >> a;
+    cout << "Number 2: " << endl;   cin >> b;
+
+    c = a - b;
+
+    cout << "Subtracted result = " << c << endl;
+}
+
+int multiplication()
+{
+    cout << "Input two number that will be multiplied together" << endl;
+
+    cout << "Number 1: " << endl;   cin >> a;
+    cout << "Number 2: " << endl;   cin >> b;
+
+    c = a * b;
+
+    cout << "Multiplied result = " << c << endl;
+}
+
+int division()
+{
+    cout << "Input two number that will be divided together" << endl;
+
+    cout << "Number 1: " << endl;   cin >> a;
+    cout << "Number 2: " << endl;   cin >> b;
+
+    c = a / b;
+
+    cout << "Divided result = " << c << endl;
+}
+
+int main()
+{
+    addition();
+
+    subtraction();
+
+
+
+    return 0;
+}
