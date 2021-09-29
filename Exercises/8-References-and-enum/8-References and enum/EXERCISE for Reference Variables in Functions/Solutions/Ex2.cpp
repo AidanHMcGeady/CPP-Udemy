5c5871a174c8bd705033ad54cd49b69e277dc004
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-29 03:16:07 +0100

    29/09 #1

--- Exercises/8-References-and-enum/8-References and enum/EXERCISE for Reference Variables in Functions/Solutions/Ex2.cpp
+++ Exercises/8-References-and-enum/8-References and enum/EXERCISE for Reference Variables in Functions/Solutions/Ex2.cpp
@@ -2,63 +2,72 @@
 
 using namespace std;
 
-int a;
-int b;
-int &c = a;
-int &d = b;
+int a, b, &c = a, &d = b;
 
 int choice1;
 int choice2;
-
 int equality;
+int combo;
 
 int checkIfTheSame()
 {
-    if(choice1 == 1 && choice2 == 2)
+    cout << "First Number = "; cin >> choice1;
+
+    cout << "Second Number = "; cin >> choice2;
+
+    ((1, 2) || (2, 1)) == true ? combo = 12 : NULL;
+    ((1, 3) || (3, 1)) == true ? combo = 13 : NULL;
+    ((1, 4) || (4, 1)) == true ? combo = 14 : NULL;
+    ((3, 2) || (2, 3)) == true ? combo = 23 : NULL;
+    ((4, 2) || (2, 4)) == true ? combo = 24 : NULL;
+    ((4, 3) || (3, 4)) == true ? combo = 34 : NULL;
+
+    switch(combo)
     {
-        a == b ? equality++, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
-        &a == &b ? equality++, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
+    case 12:
+        a == b ? ++equality, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
+        &a == &b ? ++equality, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
         equality == 2 ? cout << "Result: " << endl << "Perfect Match" << endl: cout << endl;
+        equality == 1 ? cout << "Result: " << endl << "Partial Match" << endl: cout << endl;
         equality == 0 ? cout << "Result: " << endl << "Completely Different" << endl : cout << endl;
-    }
-    else if(choice1 == 1 && choice2 == 3)
-    {
-        a == c ? equality++, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
-        &a == &c ? equality++, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
+        break;
+    case 13:
+        a == c ? ++equality, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
+        &a == &c ? ++equality, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
         equality == 2 ? cout << "Result: " << endl << "Perfect Match" << endl: cout << endl;
+        equality == 1 ? cout << "Result: " << endl << "Partial Match" << endl: cout << endl;
         equality == 0 ? cout << "Result: " << endl << "Completely Different" << endl : cout << endl;
-    }
-    else if(choice1 == 1 && choice2 == 4)
-    {
-        a == d ? equality++, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
-        &a == &d ? equality++, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
+        break;
+    case 14:
+        a == d ? ++equality, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
+        &a == &d ? ++equality, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
         equality == 2 ? cout << "Result: " << endl << "Perfect Match" << endl: cout << endl;
+        equality == 1 ? cout << "Result: " << endl << "Partial Match" << endl: cout << endl;
         equality == 0 ? cout << "Result: " << endl << "Completely Different" << endl : cout << endl;
-    }
-    else if(choice1 == 2 && choice2 == 3)
-    {
-        b == c ? equality++, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
-        &b == &c ? equality++, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
+        break;
+    case 23:
+        c == b ? ++equality, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
+        &c == &b ? ++equality, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
         equality == 2 ? cout << "Result: " << endl << "Perfect Match" << endl: cout << endl;
+        equality == 1 ? cout << "Result: " << endl << "Partial Match" << endl: cout << endl;
         equality == 0 ? cout << "Result: " << endl << "Completely Different" << endl : cout << endl;
-    }
-    else if(choice1 == 2 && choice2 == 4)
-    {
-        b == d ? equality++, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
-        &b == &d ? equality++, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
+        break;
+    case 24:
+        d == b ? ++equality, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
+        &d == &b ? ++equality, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
         equality == 2 ? cout << "Result: " << endl << "Perfect Match" << endl: cout << endl;
+        equality == 1 ? cout << "Result: " << endl << "Partial Match" << endl: cout << endl;
         equality == 0 ? cout << "Result: " << endl << "Completely Different" << endl : cout << endl;
-    }
-    else if(choice1 == 3 && choice2 == 4)
-    {
-        c == d ? equality++, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
-        &c == &d ? equality++, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
+        break;
+    case 34:
+        d == c ? ++equality, cout << "Values Match" << endl : cout << "Values Do Not Match" << endl;
+        &d == &c ? ++equality, cout << "Addresses Match" << endl : cout << "Addresses Do Not Match" << endl;
         equality == 2 ? cout << "Result: " << endl << "Perfect Match" << endl: cout << endl;
+        equality == 1 ? cout << "Result: " << endl << "Partial Match" << endl: cout << endl;
         equality == 0 ? cout << "Result: " << endl << "Completely Different" << endl : cout << endl;
-    }
-    else
-    {
-        cout << "You failed to choose correctly" << endl;
+        break;
+    default:
+        cout << "bye";
     }
 }
 
@@ -75,6 +84,5 @@ int main()
     << "3. C" << endl
     << "4. D" << endl;
 
-    cout << "First Number = "; cin >> choice1;
-    cout << "Second Number = "; cin >> choice2;
+    checkIfTheSame();
 }
